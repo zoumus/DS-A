75d0ff59aa0bb36c8108ce84eee6672f1c98d2ce
zehour <zehourchaoui@gmail.com>
2022-11-30 15:30:57 -0800

    inunivaluelist

--- isUnivalueList.js
+++ isUnivalueList.js
@@ -60,3 +60,20 @@
 
 // isUnivalueList(z); // true
 
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
+
+const isUnivalueList = (head) => {
+    let current = head;
+    
+    while(current !== null) {
+      if (current.val !== head.val) return false;
+      current = current.next;
+    }
+    return true;
+};
+  
