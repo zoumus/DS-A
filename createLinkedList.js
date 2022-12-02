f1823394aeba0c04e9b282ce129915bff9d6bc38
zehour <zehourchaoui@gmail.com>
2022-12-01 20:15:23 -0800

    createlinkedlist

---
+++ createLinkedList.js
@@ -0,0 +1,15 @@
+// Write a function, createLinkedList, that takes in an array of values as an argument. The function should create a linked list containing each element of the array as the values of the nodes. The function should return the head of the linked list.
+
+// test_00:
+// createLinkedList(["h", "e", "y"]);
+// // h -> e -> y
+// test_01:
+// createLinkedList([1, 7, 1, 8]);
+// // 1 -> 7 -> 1 -> 8
+// test_02:
+// createLinkedList(["a"]);
+// // a
+// test_03:
+// createLinkedList([]);
+// // null
+
