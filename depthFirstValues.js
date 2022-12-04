1a55f91fa16337a144ae9fc47e43e80fa183ccce
zehour <zehourchaoui@gmail.com>
2022-12-03 23:58:52 -0800

    depthfirstvalue

--- depthFirstValues.js
+++ depthFirstValues.js
@@ -81,3 +81,25 @@
 // test_04:
 // depthFirstValues(null); 
 // //    -> []
+
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
+
+const depthFirstValues = (root) => {
+    // if(root === null) return [];
+    const result = [];
+    const stack = [ root ];
+    while(stack.length > 0 && root !== null) {
+      const current = stack.pop();
+      result.push(current.val);
+      
+      if(current.right) stack.push(current.right);
+      if(current.left) stack.push(current.left);
+    }
+    return result
+  };
