12cab4101a86abd0700654532205b520eccc3bdd
zehour <zehourchaoui@gmail.com>
2022-12-05 23:25:22 -0800

    treeincludes

---
+++ treeIncludes.js
@@ -0,0 +1,135 @@
+// Write a function, treeIncludes, that takes in the root of a binary tree and a target value. The function should return a boolean indicating whether or not the value is contained in the tree.
+
+// test_00:
+// const a = new Node("a");
+// const b = new Node("b");
+// const c = new Node("c");
+// const d = new Node("d");
+// const e = new Node("e");
+// const f = new Node("f");
+
+// a.left = b;
+// a.right = c;
+// b.left = d;
+// b.right = e;
+// c.right = f;
+
+// //      a
+// //    /   \
+// //   b     c
+// //  / \     \
+// // d   e     f
+
+// treeIncludes(a, "e"); // -> true
+// test_01:
+// const a = new Node("a");
+// const b = new Node("b");
+// const c = new Node("c");
+// const d = new Node("d");
+// const e = new Node("e");
+// const f = new Node("f");
+
+// a.left = b;
+// a.right = c;
+// b.left = d;
+// b.right = e;
+// c.right = f;
+
+// //      a
+// //    /   \
+// //   b     c
+// //  / \     \
+// // d   e     f
+
+// treeIncludes(a, "a"); // -> true
+// test_02:
+// const a = new Node("a");
+// const b = new Node("b");
+// const c = new Node("c");
+// const d = new Node("d");
+// const e = new Node("e");
+// const f = new Node("f");
+
+// a.left = b;
+// a.right = c;
+// b.left = d;
+// b.right = e;
+// c.right = f;
+
+// //      a
+// //    /   \
+// //   b     c
+// //  / \     \
+// // d   e     f
+
+// treeIncludes(a, "n"); // -> false
+// test_03:
+// const a = new Node("a");
+// const b = new Node("b");
+// const c = new Node("c");
+// const d = new Node("d");
+// const e = new Node("e");
+// const f = new Node("f");
+// const g = new Node("g");
+// const h = new Node("h");
+
+// a.left = b;
+// a.right = c;
+// b.left = d;
+// b.right = e;
+// c.right = f;
+// e.left = g;
+// f.right = h;
+
+// //      a
+// //    /   \
+// //   b     c
+// //  / \     \
+// // d   e     f
+// //    /       \
+// //   g         h
+
+// treeIncludes(a, "f"); // -> true
+// test_04:
+// const a = new Node("a");
+// const b = new Node("b");
+// const c = new Node("c");
+// const d = new Node("d");
+// const e = new Node("e");
+// const f = new Node("f");
+// const g = new Node("g");
+// const h = new Node("h");
+
+// a.left = b;
+// a.right = c;
+// b.left = d;
+// b.right = e;
+// c.right = f;
+// e.left = g;
+// f.right = h;
+
+// //      a
+// //    /   \
+// //   b     c
+// //  / \     \
+// // d   e     f
+// //    /       \
+// //   g         h
+
+// treeIncludes(a, "p"); // -> false
+// test_05:
+// treeIncludes(null, "b"); // -> false
+
+const treeIncludes = (root, target) => {
+    let stack = [ root ];
+    let result = []; //
+    while (stack.length > 0 && root !== null) {
+      let current = stack.pop();
+      result.push(current.val); //if(current.val === target) return true;
+      
+      
+      if (current.left) stack.push(current.left);
+      if (current.right) stack.push(current.right);
+    }
+    return result.includes(target); // return false
+  };
