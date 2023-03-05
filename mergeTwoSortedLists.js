40c91939deff6294f1ac79c9285db5ff3d6a4e67
zehour <zehourchaoui@gmail.com>
2023-03-05 11:15:18 +0100

    paste the solution

--- mergeTwoSortedLists.js
+++ mergeTwoSortedLists.js
@@ -22,3 +22,18 @@
 // The number of nodes in both lists is in the range [0, 50].
 // -100 <= Node.val <= 100
 // Both list1 and list2 are sorted in non-decreasing order.
+
+var mergeTwoLists = function(list1, list2) {
+
+    if(list1 === null) return list2;
+    if(list2 === null) return list1;
+
+    if(list1.val < list2.val) {
+        list1.next = mergeTwoLists(list1.next, list2);
+        return list1;
+    } else {
+        list2.next = mergeTwoLists(list1, list2.next);
+        return list2;
+    }
+
+};
