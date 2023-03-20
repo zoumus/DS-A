c453c22c40ace29d3f47d39548e1c3e5dc2a3699
zehour <zehourchaoui@gmail.com>
2023-03-20 17:42:08 +0100

    finish

--- node.js
+++ node.js
@@ -5,3 +5,16 @@ class Node {
         this.right = null;
     }
 }
+
+const a = new Node('a');
+const b = new Node('b');
+const c = new Node('c');
+const d = new Node('d');
+const e = new Node('e');
+const f = new Node('f');
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
