1ffceff53f90f1b1d1eb98a7b7be1ab1e3ea4862
zehour <zehourchaoui@gmail.com>
2023-03-20 19:00:38 +0100

    ..

--- depthFirstValues.js
+++ depthFirstValues.js
@@ -90,23 +90,23 @@
 //   }
 // }
 
-// const depthFirstValues = (root) => {
-//     // if(root === null) return [];
-//     const result = [];
-//     const stack = [ root ];
-//     while(stack.length > 0 && root !== null) {
-//       const current = stack.pop();
-//       result.push(current.val);
+const depthFirstValues = (root) => {
+    // if(root === null) return [];
+    const result = [];
+    const stack = [ root ];
+    while(stack.length > 0 && root !== null) {
+      const current = stack.pop();
+      result.push(current.val);
       
-//       if(current.right) stack.push(current.right);
-//       if(current.left) stack.push(current.left);
-//     }
-//     return result
-//   };
-//  Recursive solution 
-//  const depthFirstValues = (root) => {
-//     if (root === null) return [];
-//     const leftVal = depthFirstValues(root.left);
-//     const rightVal = depthFirstValues(root.right );
-//     return [root.val, ...leftVal, ...rightVal]
-//   }
+      if(current.right) stack.push(current.right);
+      if(current.left) stack.push(current.left);
+    }
+    return result
+  };
+ Recursive solution 
+ const depthFirstValues = (root) => {
+    if (root === null) return [];
+    const leftVal = depthFirstValues(root.left);
+    const rightVal = depthFirstValues(root.right );
+    return [root.val, ...leftVal, ...rightVal]
+  }
