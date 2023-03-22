a7e53209f9e454e05fd954c55d57069cbaa5f772
zehour <zehourchaoui@gmail.com>
2023-03-22 19:11:00 +0100

    ..

--- employeeHeirarchy.js
+++ employeeHeirarchy.js
@@ -9,3 +9,26 @@
 // employeeCount(employeeHeirarchy,S) // 5
 // employeeCount(employeeHeirarchy,W) // 2
 // employeeCount(employeeHeirarchy,U) // 1
+
+function employeeCount(employeeHeirarchy,id) {
+    if(employeeHeirarchy[id].length) return 1
+    let count = 1
+    for(let neighbor of employeeHeirarchy[id]){
+        count += employeeCount(employeeHeirarchy,neighbor)
+    }
+    return count
+}
+employeeHeirarchy = {
+    	S:['T','W'],
+    	T:['U'],
+    	W:['V'],
+    	V:[],
+    	U:[]      
+}
+
+employeeCount(employeeHeirarchy,S) // 5
+employeeCount(employeeHeirarchy,W) // 2
+employeeCount(employeeHeirarchy,U) // 1
+
+
+
