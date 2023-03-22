73d3a0ee2ca0d68bc4b6116a455e48b50412959f
zehour <zehourchaoui@gmail.com>
2023-03-22 18:55:58 +0100

    paste the probblem

---
+++ employeeHeirarchy.js
@@ -0,0 +1,11 @@
+// employeeHeirarchy = {
+// 	‘S’:[‘T’,W’]
+// 	‘T’:[‘U’]
+// 	'W':[‘V’]
+// 	'V':[]
+// 	'U':[]
+// }
+// Callstack [ s: 5]
+// employeeCount(employeeHeirarchy,S) // 5
+// employeeCount(employeeHeirarchy,W) // 2
+// employeeCount(employeeHeirarchy,U) // 1
