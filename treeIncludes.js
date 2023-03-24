73ce97eecbfda1b0c68fe7ab14f5a83087ffa374
zehour <zehourchaoui@gmail.com>
2023-03-24 17:08:34 +0100

    ...

--- treeIncludes.js
+++ treeIncludes.js
@@ -133,3 +133,10 @@ const treeIncludes = (root, target) => {
     }
     return result.includes(target); // return false
   };
+
+  //Recursio: 
+  // const treeIncludes = (root, target) => {
+  //   if (root === null) return false;
+  //   if (root.val === target) return true;
+  //   return treeIncludes(root.left, target) || treeIncludes(root.right, target);
+  // };
