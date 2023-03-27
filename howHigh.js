94a3a0aa7f3a8ec647f70424517478a07ee4b5a9
zehour <zehourchaoui@gmail.com>
2023-03-27 18:43:48 +0100

    howhigh

--- howHigh.js
+++ howHigh.js
@@ -68,11 +68,11 @@
 // test_04:
 // howHigh(null); // -> -1
 
-// const howHigh = (node) => {
-//     if(node === null) return -1;
+const howHigh = (node) => {
+    if(node === null) return -1;
     
-//     let leftHigh = howHigh(node.left);
-//     let rightHigh = howHigh(node.right);
-//     return 1 + Math.max(leftHigh, rightHigh);
-// };
+    let leftHigh = howHigh(node.left);
+    let rightHigh = howHigh(node.right);
+    return 1 + Math.max(leftHigh, rightHigh);
+};
   
