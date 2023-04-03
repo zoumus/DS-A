e440ae98f63077a4bc9de615e00636a48cd6e715
zehour <zehourchaoui@gmail.com>
2023-04-03 17:34:02 +0100

    ....

--- treeLevels.js
+++ treeLevels.js
@@ -100,13 +100,13 @@ const treeLevels = (root) => {
     if (root === null) return [];
     let result = [];
     
-    let queue = [{node: root, levelNum = 0}];
+    let queue = [{node: root, levelNum: 0}];
     while(queue.length > 0) {
       const{node, levelNum} = queue.shift();
-      if (results.length === levelNum) {
+      if (result.length === levelNum) {
         result[levelNum] = [node.val];
       } else {
-        result[levelum].push(node.val);
+        result[levelNum].push(node.val);
       }
       
       if(node.left !== null)  queue.push({node: node.left, levelNum: levelNum + 1})
