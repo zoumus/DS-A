d8a548c853ab9f691f13f8bae1ffe6debde6ad1a
zehour <zehourchaoui@gmail.com>
2023-04-03 17:29:49 +0100

    ......

--- treeLevels.js
+++ treeLevels.js
@@ -94,3 +94,23 @@
 // // ]
 // test_03:
 // treeLevels(null); // -> []
+
+
+const treeLevels = (root) => {
+    if (root === null) return [];
+    let result = [];
+    
+    let queue = [{node: root, levelNum = 0}];
+    while(queue.length > 0) {
+      const{node, levelNum} = queue.shift();
+      if (results.length === levelNum) {
+        result[levelNum] = [node.val];
+      } else {
+        result[levelum].push(node.val);
+      }
+      
+      if(node.left !== null)  queue.push({node: node.left, levelNum: levelNum + 1})
+      if(node.right !== null) queue.push({node: node.right, levelNum: levelNum + 1})
+    }
+    return result;
+};
