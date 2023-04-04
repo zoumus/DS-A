09cc8e233330b5dda965dae33df0712ff9bdb4e9
zehour <zehourchaoui@gmail.com>
2023-04-04 17:43:18 +0100

    ........

--- asteroidCollision.js
+++ asteroidCollision.js
@@ -1,7 +1,7 @@
 // We are given an array asteroids of integers representing asteroids in a row.
 // For each asteroid, the absolute value represents its size, and the sign
-//  represents its direction (positive meaning right, negative meaning left). 
-//  Each asteroid moves at the same speed.
+// represents its direction (positive meaning right, negative meaning left). 
+// Each asteroid moves at the same speed.
 // Find out the state of the asteroids after all collisions. 
 // If two asteroids meet, the smaller one will explode. If both are the same size,
 // both will explode. Two asteroids moving in the same direction will never meet.
@@ -27,6 +27,9 @@
 // 2 <= asteroids.length <= 104
 // -1000 <= asteroids[i] <= 1000
 // asteroids[i] != 0
+
+// positive -> right
+// negative <- left
 
 var asteroidCollision = function(asteroids) {
     let stack = [ asteroids[0]];
