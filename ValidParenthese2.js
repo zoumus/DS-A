ae79000376e42959bc9cf579169c2b2c9fef5537
zehour <zehourchaoui@gmail.com>
2023-04-10 11:44:36 -0700

    paste the solution fron leetcode

--- ValidParenthese2.js
+++ ValidParenthese2.js
@@ -1,44 +1,44 @@
-// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
-// An input string is valid if:
-// Open brackets must be closed by the same type of brackets.
-// Open brackets must be closed in the correct order.
-// Every close bracket has a corresponding open bracket of the same type.
+// // Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+// // An input string is valid if:
+// // Open brackets must be closed by the same type of brackets.
+// // Open brackets must be closed in the correct order.
+// // Every close bracket has a corresponding open bracket of the same type.
 
-// Example 1:
+// // Example 1:
 
-// Input: s = "()"
-// Output: true
-// Example 2:
+// // Input: s = "()"
+// // Output: true
+// // Example 2:
 
-// Input: s = "()[]{}"
-// Output: true
-// Example 3:
+// // Input: s = "()[]{}"
+// // Output: true
+// // Example 3:
 
-// Input: s = "(]"
-// Output: false
+// // Input: s = "(]"
+// // Output: false
 
-// Constraints:
+// // Constraints:
 
-// 1 <= s.length <= 104
-// s consists of parentheses only '()[]{}'.
+// // 1 <= s.length <= 104
+// // s consists of parentheses only '()[]{}'.
 
-var isValid = function(s) { 
-    let stack = [];
-    let hash = {
-        '}' : '{',
-        ']' : '[',
-        ')' : '('
-    }
+// var isValid = function(s) { 
+//     let stack = [];
+//     let hash = {
+//         '}' : '{',
+//         ']' : '[',
+//         ')' : '('
+//     }
     
-    for (let i = 0; i < s.length; i++) { 
-        var input = s[i]; 
-        if (input in hash) {
-            if (stack.pop() !== hash[input] ) {
-                return false;
-            }
-        } else {
-            stack.push(input); 
-        }
-    };
-    return stack.length === 0
-}
+//     for (let i = 0; i < s.length; i++) { 
+//         var input = s[i]; 
+//         if (input in hash) {
+//             if (stack.pop() !== hash[input] ) {
+//                 return false;
+//             }
+//         } else {
+//             stack.push(input); 
+//         }
+//     };
+//     return stack.length === 0
+// }
