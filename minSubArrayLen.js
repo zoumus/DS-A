e52bd0666b5293a730cfdef3dbf46f0b0265f9f4
zehour <zehourchaoui@gmail.com>
2023-04-11 06:34:28 -0700

    new solution

--- minSubArrayLen.js
+++ minSubArrayLen.js
@@ -41,4 +41,20 @@ var minSubArrayLen = function(target, nums) {
     // if (minSub == Infinity) return 0;
     // else return minSub
     // return minSub;
+
+    let start = 0;
+    let end = 0;
+    let minValue = Infinity;
+    let subarraySum = nums[0];
+    while(start <= end && end <nums.length){
+        if(subarraySum >= target){
+            minValue = Math.min(minValue, end-start+1);
+            subarraySum -= nums[start];
+            start++;
+        } else {
+            end++;
+            subarraySum += nums[end];
+        }
+    }
+    return minValue === Infinity ? 0 : minValue;
 }
