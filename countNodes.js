efc817f33c75a580910b4b09d7136d196361bf3d
zehour <zehourchaoui@gmail.com>
2023-04-14 15:11:51 -0700

    create a new problem

--- countNodes.js
+++ countNodes.js
@@ -1,22 +1,22 @@
-Given the root of a complete binary tree, return the number of the nodes in the tree.
-According to Wikipedia, every level, except possibly the last, is completely filled in a complete binary tree, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
-Design an algorithm that runs in less than O(n) time complexity.
+// Given the root of a complete binary tree, return the number of the nodes in the tree.
+// According to Wikipedia, every level, except possibly the last, is completely filled in a complete binary tree, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+// Design an algorithm that runs in less than O(n) time complexity.
 
-Example 1:
+// Example 1:
 
-Input: root = [1,2,3,4,5,6]
-Output: 6
-Example 2:
+// Input: root = [1,2,3,4,5,6]
+// Output: 6
+// Example 2:
 
-Input: root = []
-Output: 0
-Example 3:
+// Input: root = []
+// Output: 0
+// Example 3:
 
-Input: root = [1]
-Output: 1
+// Input: root = [1]
+// Output: 1
  
-Constraints:
+// Constraints:
 
-The number of nodes in the tree is in the range [0, 5 * 104].
-0 <= Node.val <= 5 * 104
-The tree is guaranteed to be complete.
+// The number of nodes in the tree is in the range [0, 5 * 104].
+// 0 <= Node.val <= 5 * 104
+// The tree is guaranteed to be complete.
