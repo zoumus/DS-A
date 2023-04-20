3a122b1a1096d55b7af3cdd072bf8042d95d1668
zehour <zehourchaoui@gmail.com>
2023-04-20 00:55:21 -0700

    fix a problem

--- twoSum2.js
+++ twoSum2.js
@@ -30,12 +30,12 @@
 // The tests are generated such that there is exactly one solution.
 
 var twoSum = function(numbers, target) {
-    let left = 0
+    let left = 0;
     let right = numbers.length - 1;
 
     while (left < right) {
         let sum = numbers[left] + numbers[right];
-
+        
         if(sum < target) left ++;
         else if(sum > target) right --;
         else return [left+1, right+1]
