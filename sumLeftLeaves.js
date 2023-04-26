c490e5374a8cbd6a0458f43952ca22dd37881103
zehour <zehourchaoui@gmail.com>
2023-04-25 21:47:06 -0700

    new problem

--- sumLeftLeaves.js
+++ sumLeftLeaves.js
@@ -1,23 +1,23 @@
-Given the root of a binary tree, return the sum of all left leaves.
+// Given the root of a binary tree, return the sum of all left leaves.
 
-A leaf is a node with no children. A left leaf is a leaf that is the left child of another node.
+// A leaf is a node with no children. A left leaf is a leaf that is the left child of another node.
 
-Example 1:
+// Example 1:
 
 
-Input: root = [3,9,20,null,null,15,7]
-Output: 24
-Explanation: There are two left leaves in the binary tree, with values 9 and 15 respectively.
-Example 2:
+// Input: root = [3,9,20,null,null,15,7]
+// Output: 24
+// Explanation: There are two left leaves in the binary tree, with values 9 and 15 respectively.
+// Example 2:
 
-Input: root = [1]
-Output: 0
+// Input: root = [1]
+// Output: 0
  
 
-Constraints:
+// Constraints:
 
-The number of nodes in the tree is in the range [1, 1000].
--1000 <= Node.val <= 1000
+// The number of nodes in the tree is in the range [1, 1000].
+// -1000 <= Node.val <= 1000
 
 var sumOfLeftLeaves = function(root) {
     let sum = 0;
