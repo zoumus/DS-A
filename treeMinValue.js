e78b3e4288b67f156a43658b727fe79b9f118290
zehour <zehourchaoui@gmail.com>
2023-05-01 22:21:26 -0700

    subset

--- treeMinValue.js
+++ treeMinValue.js
@@ -1,13 +1,13 @@
-// Write a function, treeMinValue, that takes in the root of a binary tree that contains number values. The function should return the minimum value within the tree.
-// You may assume that the input tree is non-empty.
+Write a function, treeMinValue, that takes in the root of a binary tree that contains number values. The function should return the minimum value within the tree.
+You may assume that the input tree is non-empty.
 
-// test_00:
-// const a = new Node(3);
-// const b = new Node(11);
-// const c = new Node(4);
-// const d = new Node(4);
-// const e = new Node(-2);
-// const f = new Node(1);
+test_00:
+const a = new Node(3);
+const b = new Node(11);
+const c = new Node(4);
+const d = new Node(4);
+const e = new Node(-2);
+const f = new Node(1);
 
 // a.left = b;
 // a.right = c;
