ac5f6e3b7fae14d5cce2f152e30c69b6146a78f6
zehour <zehourchaoui@gmail.com>
2023-05-08 13:18:19 -0700

    fizzbuzz problem

---
+++ fizzbuzz.js
@@ -0,0 +1,28 @@
+/*
+ * Complete the 'fizzbuzz' function below.
+ *
+ * The function is expected to return a STRING_ARRAY.
+ * The function accepts INTEGER n as parameter.
+ */
+
+function fizzbuzz(n) {
+    let answer = [];
+    for(let i = 1; i <= n; i ++){
+        // if (i === 0) continue;
+        if(i % 3 === 0 && i % 5 === 0) {
+            answer.push('FizzBuzz');
+            continue;
+        } else if(i % 3 === 0) {
+            answer.push('Fizz')
+        } else if(i % 5 === 0) {
+            answer.push('Buzz')
+            
+        } else {
+            answer.push(i.toString())
+        }
+    }
+    return answer;
+}
+
+//n = 3 => []
+// = 5 => 
