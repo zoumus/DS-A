968b821951e7cf4b46af42669e46b1ea2a159ed9
zehour <zehourchaoui@gmail.com>
2023-05-10 02:00:01 -0700

    sortColors problem

--- sortColors.js
+++ sortColors.js
@@ -1,10 +1,6 @@
 Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
-
 We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
-
 You must solve this problem without using the library's sort function.
-
- 
 
 Example 1:
 
@@ -15,7 +11,6 @@ Example 2:
 Input: nums = [2,0,1]
 Output: [0,1,2]
  
-
 Constraints:
 
 n == nums.length
