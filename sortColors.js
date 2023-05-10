67e2e742bbfdf187d615fe35c325196b2dc3b456
zehour <zehourchaoui@gmail.com>
2023-05-10 02:01:05 -0700

    sortColors problem

--- sortColors.js
+++ sortColors.js
@@ -33,4 +33,14 @@ var sortColors = function(nums) {
     //     }
     // }
     // return nums
+
+    let zeros = 0, ones = 0, twos = 0;
+
+    for(let n of nums){
+        n === 0 ? zeros++ : n === 1 ? ones++ : twos++;
+    }
+    for(let i = 0; i < nums.length; i++){
+        i < zeros ? nums[i] = 0 : i < zeros+ones ? nums[i] = 1 : nums[i] = 2;
+    }
+    return nums;
 }
