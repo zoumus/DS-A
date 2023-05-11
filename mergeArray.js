d64fb9dacfbd73b1b1241b5d2864c59abd281820
zehour <zehourchaoui@gmail.com>
2023-05-11 00:44:57 -0700

    mergeArray prolem

--- mergeArray.js
+++ mergeArray.js
@@ -16,3 +16,25 @@
 
 // 1 <= nums.length <= 5 * 104
 // -5 * 104 <= nums[i] <= 5 * 104
+
+var sortArray = function(nums) {
+    if(nums.length < 2) return nums;
+
+    let mid = Math.floor(nums.length/2);
+    let left = nums.slice(0, mid);
+    let right = nums.slice(mid);
+
+    return merges(sortArray(left), sortArray(right));
+};
+const merges = (left, right) => {
+    let merged = [];
+    while(left.length && right.length) {
+        if (left[0] > right[0]) {
+            merged.push(right.shift());
+        } else {
+            merged.push(left.shift());
+        }
+    }
+    
+    return [...merged, ...left, ...right];
+}
