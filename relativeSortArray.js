9c7d951e5a4e35676b2052b9ec3d396fa0f7deb6
zehour <zehourchaoui@gmail.com>
2023-05-15 23:55:38 -0700

    relativeSortArray  prolem

--- relativeSortArray.js
+++ relativeSortArray.js
@@ -1,8 +1,5 @@
 Given two arrays arr1 and arr2, the elements of arr2 are distinct, and all elements in arr2 are also in arr1.
-
 Sort the elements of arr1 such that the relative ordering of items in arr1 are the same as in arr2. Elements that do not appear in arr2 should be placed at the end of arr1 in ascending order.
-
- 
 
 Example 1:
 
@@ -13,7 +10,6 @@ Example 2:
 Input: arr1 = [28,6,22,8,44,17], arr2 = [22,28,8,6]
 Output: [22,28,8,6,17,44]
  
-
 Constraints:
 
 1 <= arr1.length, arr2.length <= 1000
