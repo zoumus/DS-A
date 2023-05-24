1de43b5962b214cc96af4bb67d2ea2af3f476885
zehour <zehourchaoui@gmail.com>
2023-05-23 17:53:33 -0700

    made some changes in remove nth from linkedlist problem

--- removeNthFromEnd.js
+++ removeNthFromEnd.js
@@ -35,5 +35,19 @@ var removeNthFromEnd = function(head, n) {
     }
     if ( count + 1 === n) return slow.next;
     slow.next = slow.next.next
-    return head
+    return head;
+
+    // let fast = head, slow = head;
+
+    // for(let i = 0; i < n; i ++) {
+    //     fast = fast.next;
+    // };
+
+    // if(!fast) return slow.next;
+    // while(fast !== null && fast.next !== null) {
+    //     fast = fast.next;
+    //     slow = slow.next;
+    // }
+    // slow.next = slow.next.next;
+    // return head;
 };
