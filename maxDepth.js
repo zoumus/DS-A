900b867cb4a5f3da6aabe788606e59364204326a
zehour <zehourchaoui@gmail.com>
2023-05-28 22:17:26 -0700

    maxDepth problem

--- maxDepth.js
+++ maxDepth.js
@@ -13,7 +13,7 @@
 var maxDepth = function(root) {
     if (!root) return 0;
     let left = maxDepth(root.left);
-    let right = maxDepth(root.right);
+    let right = maxDepth(root.right)
 
     return 1 + Math.max(left, right);
 };
