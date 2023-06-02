6846a23386996c84512903853bc11e27eece8ac0
zehour <zehourchaoui@gmail.com>
2023-06-01 23:58:33 -0700

    copyNode problem

--- copyNode.js
+++ copyNode.js
@@ -1,7 +1,5 @@
 Given a reference of a node in a connected undirected graph.
-
 Return a deep copy (clone) of the graph.
-
 Each node in the graph contains a value (int) and a list (List[Node]) of its neighbors.
 
 class Node {
@@ -9,19 +7,14 @@ class Node {
     public List<Node> neighbors;
 }
  
-
 Test case format:
 
 For simplicity, each node's value is the same as the node's index (1-indexed). For example, the first node with val == 1, the second node with val == 2, and so on. The graph is represented in the test case using an adjacency list.
-
 An adjacency list is a collection of unordered lists used to represent a finite graph. Each list describes the set of neighbors of a node in the graph.
-
 The given node will always be the first node with val = 1. You must return the copy of the given node as a reference to the cloned graph.
 
- 
 
 Example 1:
-
 
 Input: adjList = [[2,4],[1,3],[2,4],[1,3]]
 Output: [[2,4],[1,3],[2,4],[1,3]]
@@ -32,7 +25,6 @@ Explanation: There are 4 nodes in the graph.
 4th node (val = 4)'s neighbors are 1st node (val = 1) and 3rd node (val = 3).
 Example 2:
 
-
 Input: adjList = [[]]
 Output: [[]]
 Explanation: Note that the input contains one empty list. The graph consists of only one node with val = 1 and it does not have any neighbors.
@@ -42,7 +34,6 @@ Input: adjList = []
 Output: []
 Explanation: This an empty graph, it does not have any nodes.
  
-
 Constraints:
 
 The number of nodes in the graph is in the range [0, 100].
