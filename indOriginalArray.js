94972cc89f44b3a7fd3816fe2c50169acbce22bb
zehour <zehourchaoui@gmail.com>
2023-06-06 22:23:38 -0700

    indOriginalArray problem

--- indOriginalArray.js
+++ indOriginalArray.js
@@ -1,8 +1,5 @@
 An integer array original is transformed into a doubled array changed by appending twice the value of every element in original, and then randomly shuffling the resulting array.
-
 Given an array changed, return original if changed is a doubled array. If changed is not a doubled array, return an empty array. The elements in original may be returned in any order.
-
- 
 
 Example 1:
 
@@ -24,7 +21,6 @@ Input: changed = [1]
 Output: []
 Explanation: changed is not a doubled array.
  
-
 Constraints:
 
 1 <= changed.length <= 105
