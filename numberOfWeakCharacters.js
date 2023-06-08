e11429fc04fd067330731bda416473364a57ebf1
zehour <zehourchaoui@gmail.com>
2023-06-07 23:13:17 -0700

    numberOfWeakCharacters prolem

--- numberOfWeakCharacters.js
+++ numberOfWeakCharacters.js
@@ -1,10 +1,6 @@
 You are playing a game that contains multiple characters, and each of the characters has two main properties: attack and defense. You are given a 2D integer array properties where properties[i] = [attacki, defensei] represents the properties of the ith character in the game.
-
 A character is said to be weak if any other character has both attack and defense levels strictly greater than this character's attack and defense levels. More formally, a character i is said to be weak if there exists another character j where attackj > attacki and defensej > defensei.
-
 Return the number of weak characters.
-
- 
 
 Example 1:
 
@@ -22,7 +18,6 @@ Input: properties = [[1,5],[10,4],[4,3]]
 Output: 1
 Explanation: The third character is weak because the second character has a strictly greater attack and defense.
  
-
 Constraints:
 
 2 <= properties.length <= 105
