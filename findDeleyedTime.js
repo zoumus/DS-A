b768d68f0af5030b718925984d8040769c491345
zehour <zehourchaoui@gmail.com>
2023-06-11 00:22:20 -0700

    findDeleyedTime

---
+++ findDeleyedTime.js
@@ -0,0 +1,9 @@
+function findDelayedArrivalTime(arrivalTime, delayedTime) {
+    let max = arrivalTime + delayedTime;
+    if(max === 24) {
+        return 0
+    } else if(max > 24) {
+        return max - 24;
+    } else {
+        return max;
+    }
