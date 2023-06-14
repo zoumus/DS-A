830871c0109e9ae3f873a9cbc9aab268ba129f99
zehour <zehourchaoui@gmail.com>
2023-06-14 00:13:29 -0700

    hasPath problem

--- hasPath.js
+++ hasPath.js
@@ -70,11 +70,9 @@ const hasPath = (graph, src, dst) => {
 //recursion
 
 // if(src === dst) return true;
-  
 // for(let neighbor of graph[src]) {
 //   if(hasPath(graph, neighbor, dst) === true) return true;
 // }
 // return false;
-  
 };
   
