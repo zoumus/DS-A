1aacc460b831eeb51aea694cc3e33593303a90df
zehour <zehourchaoui@gmail.com>
2023-06-22 22:52:30 -0700

    merge problem

--- merge.js
+++ merge.js
@@ -10,9 +10,6 @@
 // Input: intervals = [[1,4],[4,5]]
 // Output: [[1,5]]
 // Explanation: Intervals [1,4] and [4,5] are considered overlapping.
- 
-
-
 // 1 <= intervals.length <= 104
 // intervals[i].length == 2
 // 0 <= starti <= endi <= 104
