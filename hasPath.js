b01b21c5f1eaf1fa70394fda1c858aa72d22617c
zehour <zehourchaoui@gmail.com>
2023-06-25 22:45:09 -0700

    hasPath problem

--- hasPath.js
+++ hasPath.js
@@ -56,17 +56,17 @@
 // hasPath(graph, 'v', 'z'); // false
 
 const hasPath = (graph, src, dst) => {
-  // let queue = [src];
+  let queue = [src];
   
-  // while(queue.length) {
-  //   let current = queue.shift();
-  //   if (current === dst) return true;
+  while(queue.length) {
+    let current = queue.shift();
+    if (current === dst) return true;
     
-  //   for(let neighbor of graph[current]) {
-  //   queue.push(neighbor)
-  //   }
-  // }
-  // return false;
+    for(let neighbor of graph[current]) {
+    queue.push(neighbor)
+    }
+  }
+  return false;
 //recursion
 
 // if(src === dst) return true;
