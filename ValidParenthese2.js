98f54b878f63e3e545730c07ad286eed9f0c8740
zehour <zehourchaoui@gmail.com>
2023-06-26 21:58:40 -0700

    validPratheses problem

--- ValidParenthese2.js
+++ ValidParenthese2.js
@@ -36,9 +36,7 @@ var isValid = function(s) {
             if (stack.pop() !== hash[input] ) {
                 return false;
             }
-        } else {
-            stack.push(input); 
-        }
+        } 
     };
     return stack.length === 0
 }
