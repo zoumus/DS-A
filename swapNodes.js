ee29f3dea3b552d75ab462542b9c4486b703bf1e
zehour <zehourchaoui@gmail.com>
2023-07-12 23:49:01 -0700

    swapeNodes problem

--- swapNodes.js
+++ swapNodes.js
@@ -29,6 +29,6 @@ var swapNodes = function(head, k) {
         curr = curr.next;
         right = right.next;
     }
-    [left, right] = [right, left];
+    [left., right] = [right, left];
     return head;
 };
