bcc1c066ac0421e0f520441f19a247f3c515b057
zehour <zehourchaoui@gmail.com>
2023-07-16 00:52:23 -0700

    treeMinValue problem

--- treeMinValue.js
+++ treeMinValue.js
@@ -90,6 +90,6 @@ const treeMinValue = (root) => {
 
     Recursion
     if(!root) return Infinity;
-    if(!root.left && !root.right) return root.val;
+    // if(!root.left && !root.right) return root.val;
     // return Math.min(treeMinValue(root.left),treeMinValue(root.right), root.val)
 };
