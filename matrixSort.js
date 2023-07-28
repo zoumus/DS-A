fc4facd4d5232ab1dad1eb9d1e92245c34347271
zehour <zehourchaoui@gmail.com>
2023-07-28 00:06:33 -0700

    matrixSort problem

--- matrixSort.js
+++ matrixSort.js
@@ -13,7 +13,7 @@
 // Example 2:
 
 // Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
-Output: false
+// Output: false
 
 
 // iterate through the matrix
@@ -42,5 +42,5 @@ function searchTarget(matrix, target) {
 			}
 		}
 }
-return false;
+	return false;
 }
