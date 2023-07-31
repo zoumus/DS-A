33aaa1144b1d63fd3e113e17e77519ecbc8702f1
zehour <zehourchaoui@gmail.com>
2023-07-30 23:00:00 -0700

    merge problem

--- merge.js
+++ merge.js
@@ -7,15 +7,15 @@
 // Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
 // Example 2:
 
-Input: intervals = [[1,4],[4,5]]
-Output: [[1,5]]
-Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+// Input: intervals = [[1,4],[4,5]]
+// Output: [[1,5]]
+// Explanation: Intervals [1,4] and [4,5] are considered overlapping.
  
-Constraints:
+// Constraints:
 
-1 <= intervals.length <= 104
-intervals[i].length == 2
-0 <= starti <= endi <= 104
+// 1 <= intervals.length <= 104
+// intervals[i].length == 2
+// 0 <= starti <= endi <= 104
 
 var merge = function(intervals) {
     intervals.sort((a, b) => a[0] - b[0]);
