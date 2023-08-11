d8666538be26081dc432c801bc82d2031ac9f2b4
zehour <zehourchaoui@gmail.com>
2023-08-11 14:39:55 -0700

    longestConsecutive problem

--- longestConsecutive.js
+++ longestConsecutive.js
@@ -8,8 +8,8 @@
 // Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
 // Example 2:
 
-Input: nums = [0,3,7,2,5,8,4,6,0,1]
-Output: 9
+// Input: nums = [0,3,7,2,5,8,4,6,0,1]
+// Output: 9
  
 Constraints:
 0 <= nums.length <= 105
