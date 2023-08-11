fdbbc78a3595cf6f43aad478488928535b9dbe3f
zehour <zehourchaoui@gmail.com>
2023-08-11 14:39:04 -0700

    longestConsecutive problem

--- longestConsecutive.js
+++ longestConsecutive.js
@@ -1,7 +1,7 @@
 // Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
 // You must write an algorithm that runs in O(n) time.
 
-// Example 1:
+Example 1:
 
 Input: nums = [100,4,200,1,3,2]
 Output: 4
