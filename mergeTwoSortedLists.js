bae21bfa288f44afd75b54baf8b016ac3cfb3444
zehour <zehourchaoui@gmail.com>
2023-08-23 23:30:00 -0700

    mergeTwoSorted problem

--- mergeTwoSortedLists.js
+++ mergeTwoSortedLists.js
@@ -22,10 +22,10 @@
 // -100 <= Node.val <= 100
 // Both list1 and list2 are sorted in non-decreasing order.
 
-// var mergeTwoLists = function(list1, list2) {
+var mergeTwoLists = function(list1, list2) {
 
-//     if(list1 === null) return list2;
-//     if(list2 === null) return list1;
+    if(list1 === null) return list2;
+    if(list2 === null) return list1;
 
     // if(list1.val < list2.val) {
     //     list1.next = mergeTwoLists(list1.next, list2);
