dc3500e2a2304a01090eacaa2a5914f04bca71f4
zehour <zehourchaoui@gmail.com>
2023-08-24 23:28:03 -0700

    middleNode problem

--- middleNode.js
+++ middleNode.js
@@ -17,24 +17,24 @@
 // The number of nodes in the list is in the range [1, 100].
 // 1 <= Node.val <= 100
 
-var middleNode = function(head) {
-    let slow = head;
-    let fast = head;
-
-    while(fast !== null && fast.next !== null){
-        slow = pointer1.next;
-        fast = pointer2.next.next;
-    }
-    return slow;
-};
-
 // var middleNode = function(head) {
-//     let pointer1 = head;
-//     let pointer2 = head;
+//     let slow = head;
+//     let fast = head;
 
-//     while(pointer2 !== null && pointer2.next !== null){
-//         pointer1 = pointer1.next;
-//         pointer2 = pointer2.next.next;
+//     while(fast !== null && fast.next !== null){
+//         slow = pointer1.next;
+//         fast = pointer2.next.next;
 //     }
-//     return pointer1;
+//     return slow;
 // };
+
+var middleNode = function(head) {
+    let pointer1 = head;
+    let pointer2 = head;
+
+    while(pointer2 !== null && pointer2.next !== null){
+        pointer1 = pointer1.next;
+        pointer2 = pointer2.next.next;
+    }
+    return pointer1;
+};
