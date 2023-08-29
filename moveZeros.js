97911015f7b9fac97652fdf82738b63c5978b597
zehour <zehourchaoui@gmail.com>
2023-08-28 22:11:57 -0700

    moveZeroes problem

--- moveZeros.js
+++ moveZeros.js
@@ -10,12 +10,12 @@
 // Input: nums = [0]
 // Output: [0]
 
-var moveZeroes = function(nums) {
-    let index = 0;
-    for(let i = 0; i < nums.length; i++) {
-        if(nums[i] !== 0) { // [1,3,12,0,0]
-            nums[index] = nums[i];
-            index ++; //index = 3
+// var moveZeroes = function(nums) {
+//     let index = 0;
+//     for(let i = 0; i < nums.length; i++) {
+//         if(nums[i] !== 0) { // [1,3,12,0,0]
+//             nums[index] = nums[i];
+//             index ++; //index = 3
         // } 
 //     }
 //     for(let i = index; i < nums.length; i ++) {
