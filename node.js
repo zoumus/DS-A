f6e19e6983e49a8f51ab6948d5c668d8e3da1ae3
zehour <zehourchaoui@gmail.com>
2023-08-30 23:14:47 -0700

    node problem

--- node.js
+++ node.js
@@ -6,12 +6,12 @@ class Node {
     }
 }
 
-const a = new Node('a');
-const b = new Node('b');
-const c = new Node('c');
-const d = new Node('d');
-const e = new Node('e');
-const f = new Node('f');
+// const a = new Node('a');
+// const b = new Node('b');
+// const c = new Node('c');
+// const d = new Node('d');
+// const e = new Node('e');
+// const f = new Node('f');
 
 // a.left = b;
 // a.right = c;
