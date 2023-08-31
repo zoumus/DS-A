73fd85068f9ce489746d401d43a4933260fffd3a
zehour <zehourchaoui@gmail.com>
2023-08-30 23:14:40 -0700

    node problem

--- node.js
+++ node.js
@@ -13,8 +13,8 @@ const d = new Node('d');
 const e = new Node('e');
 const f = new Node('f');
 
-a.left = b;
-a.right = c;
-b.left = d;
-b.right = e;
-c.right = f;
+// a.left = b;
+// a.right = c;
+// b.left = d;
+// b.right = e;
+// c.right = f;
