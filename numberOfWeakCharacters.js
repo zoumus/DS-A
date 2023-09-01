61d79ce09fde6edc23d385e751cbdfd9be8203b2
zehour <zehourchaoui@gmail.com>
2023-08-31 23:30:14 -0700

    numberOf problem

--- numberOfWeakCharacters.js
+++ numberOfWeakCharacters.js
@@ -14,9 +14,9 @@
 // Explanation: The first character is weak because the second character has a strictly greater attack and defense.
 // Example 3:
 
-// Input: properties = [[1,5],[10,4],[4,3]]
-// Output: 1
-// Explanation: The third character is weak because the second character has a strictly greater attack and defense.
+Input: properties = [[1,5],[10,4],[4,3]]
+Output: 1
+Explanation: The third character is weak because the second character has a strictly greater attack and defense.
  
 Constraints:
 
