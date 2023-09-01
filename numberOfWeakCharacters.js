e93cf239ac43faf0a5e98b339757d29f40045259
zehour <zehourchaoui@gmail.com>
2023-08-31 23:29:46 -0700

    numberOf problem

--- numberOfWeakCharacters.js
+++ numberOfWeakCharacters.js
@@ -24,12 +24,12 @@
 // properties[i].length == 2
 // 1 <= attacki, defensei <= 105
 
-var numberOfWeakCharacters = function(properties) {
-    properties.sort((a,b) => (a[0] == b[0]) ? (a[1]-b[1]) : (b[0]-a[0]))
-    let count = 0, max = 0
-    for(let arr of properties){
-        if(arr[1] < max)    count++
-        max = Math.max(max, arr[1])
-    }
-    return count
-};
+// var numberOfWeakCharacters = function(properties) {
+//     properties.sort((a,b) => (a[0] == b[0]) ? (a[1]-b[1]) : (b[0]-a[0]))
+//     let count = 0, max = 0
+//     for(let arr of properties){
+//         if(arr[1] < max)    count++
+//         max = Math.max(max, arr[1])
+//     }
+//     return count
+// };
