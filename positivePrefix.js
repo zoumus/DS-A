6fc99de5d156944f26eb865f33f062d14282df4a
zehour <zehourchaoui@gmail.com>
2023-09-06 15:30:25 -0700

    pathFinder problem

--- positivePrefix.js
+++ positivePrefix.js
@@ -1,14 +1,14 @@
-function maxPosPrefixes(arr) {
-    arr = arr.sort((a,b)=> b-a);
-    let pSumArr = [];
-    let pSum = 0;
-    let posNum = 0;
-    for(let i = 0; i < arr.length; i++) {
-        pSum += arr[i];
-        pSumArr.push(pSum);
-    }
-    for(let i = 0; i < pSumArr.length; i++) {
-        if(pSumArr[i] > 0) posNum ++;
-    }
-    return posNum;
-}
+// function maxPosPrefixes(arr) {
+//     arr = arr.sort((a,b)=> b-a);
+//     let pSumArr = [];
+//     let pSum = 0;
+//     let posNum = 0;
+//     for(let i = 0; i < arr.length; i++) {
+//         pSum += arr[i];
+//         pSumArr.push(pSum);
+//     }
+//     for(let i = 0; i < pSumArr.length; i++) {
+//         if(pSumArr[i] > 0) posNum ++;
+//     }
+//     return posNum;
+// }
