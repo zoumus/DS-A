43619f32eaf29d40c8337d3d6cc002a5b5a322dd
zehour <zehourchaoui@gmail.com>
2023-09-13 23:32:56 -0700

    sql2 problem

--- sql1.sql
+++ sql1.sql
@@ -79,9 +79,9 @@ Products with product_id = 5 is ordered in February a total of (50 + 50) = 100.
 
 # Write your MySQL query statement below
 
-select product_name, Sum(unit) As unit
-from products
-join orders on products.product_id = orders.product_id
-where year(order_date) = 2020 and month(order_date) = 02
-group by orders.product_id
-having Sum(unit) >= 100
+-- select product_name, Sum(unit) As unit
+-- from products
+-- join orders on products.product_id = orders.product_id
+-- where year(order_date) = 2020 and month(order_date) = 02
+-- group by orders.product_id
+-- having Sum(unit) >= 100
