67896b89ee9009ae95f3177472c9e30fc4d7dba4
zehour <zehourchaoui@gmail.com>
2023-09-13 23:33:15 -0700

    sql2 problem

--- sql1.sql
+++ sql1.sql
@@ -81,7 +81,7 @@ Products with product_id = 5 is ordered in February a total of (50 + 50) = 100.
 
 select product_name, Sum(unit) As unit
 from products
--- join orders on products.product_id = orders.product_id
+join orders on products.product_id = orders.product_id
 -- where year(order_date) = 2020 and month(order_date) = 02
 -- group by orders.product_id
 -- having Sum(unit) >= 100
