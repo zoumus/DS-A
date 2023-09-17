a83f25b1420a6ab762123e3bd2e4e2903d6fc21e
zehour <zehourchaoui@gmail.com>
2023-09-16 23:42:05 -0700

    sumLeftLeave problem

--- sumLeftLeaves.js
+++ sumLeftLeaves.js
@@ -19,9 +19,9 @@
 // The number of nodes in the tree is in the range [1, 1000].
 // -1000 <= Node.val <= 1000
 
-var sumOfLeftLeaves = function(root) {
-    let sum = 0;
-	let stack = [root];
+// var sumOfLeftLeaves = function(root) {
+//     let sum = 0;
+// 	let stack = [root];
 
 // 	while(stack.length > 0) {
 		
