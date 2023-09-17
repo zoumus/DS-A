25508134588240fdca74845d982cbcb9530e6acd
zehour <zehourchaoui@gmail.com>
2023-09-17 00:12:04 -0700

    sumLeftLeave problem

--- sumList.js
+++ sumList.js
@@ -1,16 +1,16 @@
 // Write a function, sumList, that takes in the head of a linked list containing numbers as an argument. The function should return the total sum of all values in the linked list.
 
 // test_00:
-const a = new Node(2);
-const b = new Node(8);
-const c = new Node(3);
-const d = new Node(-1);
-const e = new Node(7);
+// const a = new Node(2);
+// const b = new Node(8);
+// const c = new Node(3);
+// const d = new Node(-1);
+// const e = new Node(7);
 
-a.next = b;
-b.next = c;
-c.next = d;
-d.next = e;
+// a.next = b;
+// b.next = c;
+// c.next = d;
+// d.next = e;
 
 // 2 -> 8 -> 3 -> -1 -> 7
 
