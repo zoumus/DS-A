aa58f7f4ab6ec7f1ceb62cd86743fe06d7ea691d
zehour <zehourchaoui@gmail.com>
2023-09-19 21:24:53 -0700

    swapNodes problem

--- swapNodes.js
+++ swapNodes.js
@@ -17,18 +17,18 @@
 // 1 <= k <= n <= 105
 // 0 <= Node.val <= 100
 
-var swapNodes = function(head, k) {
-    let curr = head;
-    let right = head;
+// var swapNodes = function(head, k) {
+//     let curr = head;
+//     let right = head;
     
-    for(let i = 1; i < k; i++) {
-        curr = curr.next;
-    }
-    let left = curr;
-    while(curr.next) {
-        curr = curr.next;
-        right = right.next;
-    }
-    [left.val, right.val] = [right.val, left.val];
-    return head;
-};
+//     for(let i = 1; i < k; i++) {
+//         curr = curr.next;
+//     }
+//     let left = curr;
+//     while(curr.next) {
+//         curr = curr.next;
+//         right = right.next;
+//     }
+//     [left.val, right.val] = [right.val, left.val];
+//     return head;
+// };
