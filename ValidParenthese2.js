b55650b1ca2d3488d8a1afc269b0da40861187ab
zehour <zehourchaoui@gmail.com>
2023-09-26 22:04:37 -0700

    validPr problem

--- ValidParenthese2.js
+++ ValidParenthese2.js
@@ -22,13 +22,13 @@
 // 1 <= s.length <= 104
 // s consists of parentheses only '()[]{}'.
 
-// var isValid = function(s) { 
-//     let stack = [];
-//     let hash = {
-//         '}' : '{',
-//         ']' : '[',
-//         ')' : '('
-//     }
+var isValid = function(s) { 
+    let stack = [];
+    let hash = {
+        '}' : '{',
+        ']' : '[',
+        ')' : '('
+    }
     
     for (let i = 0; i < s.length; i++) { 
         var input = s[i]; 
