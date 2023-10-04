be29301681b271ce24efa4b96ef906570190b3c9
zehour <zehourchaoui@gmail.com>
2023-10-03 22:13:36 -0700

    new problem

--- treeValueCount.js
+++ treeValueCount.js
@@ -98,17 +98,17 @@
 // test_04:
 // treeValueCount(null, 42); // -> 0
 
-// const treeValueCount = (root, target) => {
-//     if(root === null) return 0;
+const treeValueCount = (root, target) => {
+    if(root === null) return 0;
     
-//     let count = 0;
-//     let stack = [ root ];
-//     while(stack.length > 0) {
-//       let current = stack.pop();
-//       if(current.val === target) count+=1;
+    let count = 0;
+    let stack = [ root ];
+    while(stack.length > 0) {
+      let current = stack.pop();
+      if(current.val === target) count+=1;
       
-//       if(current.left) stack.push(current.left);
-//       if(current.right) stack.push(current.right);
-//     }
-//     return count;
-// };
+      if(current.left) stack.push(current.left);
+      if(current.right) stack.push(current.right);
+    }
+    return count;
+};
