41dd54b263bd86a6a5486de1b73fb963f6745692
zehour <zehourchaoui@gmail.com>
2023-10-07 22:42:20 -0700

    treeLevl problem

--- treeLevels.js
+++ treeLevels.js
@@ -102,12 +102,12 @@
     
 //     let queue = [{node: root, levelNum: 0}];
 //     while(queue.length > 0) {
-      const{node, levelNum} = queue.shift();
-      if (result.length === levelNum) {
-        result[levelNum] = [node.val];
-      } else {
-        result[levelNum].push(node.val);
-      }
+      // const{node, levelNum} = queue.shift();
+      // if (result.length === levelNum) {
+      //   result[levelNum] = [node.val];
+      // } else {
+      //   result[levelNum].push(node.val);
+      // }
       
       if(node.left !== null)  queue.push({node: node.left, levelNum: levelNum + 1})
       if(node.right !== null) queue.push({node: node.right, levelNum: levelNum + 1})
